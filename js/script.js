6d36511f3b2a3f57b993d681de67638b12c39554
Lucas Zamora <Lucaszam00@hotmail.com>
2022-07-20 11:54:35 -0300

    Arrays

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
 // SITUACION: Una nutricionista, a traves de su página web, nos pide que ingresemos los datos de cada uno de sus pacientes, y en base a eso, te devuelve que tipo de plan alimenticio deberían seguir
 
-// class Paciente {
-//     constructor(nombre="", apellido="", edad=1, altura=100, peso=10){
-//     this.nombre= nombre;
-//     this.edad= edad;
-//     this.altura= altura;
-//     this.peso= peso;
-// }
-// }
+class Paciente {
+    constructor(nombre="", apellido="", edad=1, altura=100, peso=10){
+    this.nombre= nombre;
+    this.edad= edad;
+    this.altura= altura;
+    this.peso= peso;
+}
+}
 
 
 
 
-// let nombre= (prompt("Bienvenido/a al consultorio de la Doctora Fortunato. Por favor, ingrese su mombre"))
-// let edad,altura,peso
-// do{
+let nombre= (prompt("Bienvenido/a al consultorio de la Doctora Gallardo. Por favor, ingrese su mombre"))
+let edad,altura,peso
+do{
 
-// edad= parseFloat (prompt("Ingrese su edad"))
+edad= parseFloat (prompt("Ingrese su edad"))
 
-// if(isNaN(edad)){
-//     alert("Ingrese un N° valido")
-//     }
-// }while(isNaN(edad))
+if(isNaN(edad)){
+    alert("Ingrese un N° valido")
+    }
+}while(isNaN(edad))
 
-// do{
-// altura= parseFloat (prompt("Ingrese su altura en cm, por ejemplo, si ud mide 1,78m, deberia ingresar '178'"))
+do{
+altura= parseFloat (prompt("Ingrese su altura en cm, por ejemplo, si ud mide 1,78m, deberia ingresar '178'"))
 
-// if(isNaN(altura)){
-//     alert("Ingrese un N° valido")
-//     }
-// }while(isNaN(altura))
+if(isNaN(altura)){
+    alert("Ingrese un N° valido")
+    }
+}while(isNaN(altura))
 
-// do{
-// peso= parseFloat (prompt("Ingrese su peso en kg"))
+do{
+peso= parseFloat (prompt("Ingrese su peso en kg"))
 
-// if(isNaN(peso)){
-//     alert("Ingrese un N° valido")
-//     }
-// }while(isNaN(peso))
-// do{
-//     arrayPacientes.push(new Paciente (nombre, edad, altura, peso))
-// } while(s)
+if(isNaN(peso)){
+    alert("Ingrese un N° valido")
+    }
+}while(isNaN(peso))
 
-// const arrayPacientes = []
 
-// alert (`${(nombre)}, tu plan ideál, basado en tu peso, tu altura, y tu edad, es el siguiente.`)
-// const paciente1= new Paciente( "Lucas", "Zamora", 22, 174,78)
-// console.log(paciente1)
-// console.log(Paciente) 
+const paciente1 = []
+
+paciente1.push(new Paciente (nombre, edad, altura, peso))
+
+
+alert (`${(nombre)}, tu plan ideál, basado en tu peso, tu altura, y tu edad, es el siguiente.`)
+
+console.log(paciente1)
